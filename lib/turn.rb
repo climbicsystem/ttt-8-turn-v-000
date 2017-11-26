9c6a3e3be1b95c150ba9f6071da0c940e6dc70ad
Joseph Vint <josephvint@me.com>
2017-11-26 19:15:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,5 +17,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between(0, 8) && position_taken?(board, index) == false
+  (index.between(0, 8)) && (position_taken?(board, index) == false)
 end
